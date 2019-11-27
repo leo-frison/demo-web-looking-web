2d1ecbe4f259b1746f5b08610257d4d99dd2313c
Guilherme Pontes <guilhermepontes@msn.com>
2019-11-27 15:02:42 +0100

    feat: add email api

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -1,35 +1,67 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import fs from 'fs'
+import multiparty from 'multiparty'
 import sendgrid from '@sendgrid/mail'
 const from = 'G. the bot <recruitment@techmill.nl>'
-const to = 'guilherm3pontes@gmail.com'
+const to = 'guilherme@techmill.nl'
 const templateId = 'd-2d01c331be204df39e564173070e5829'
-const apiKey = ''
+const apiKey: string = process.env.SENDGRID_API_KEY || ''
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'POST') {
-    return res.status(404).json({
-      error: 'API call not found'
-    })
-  }
+function encode(file: any) {
+  var bitmap = fs.readFileSync(file)
+  return new Buffer(bitmap).toString('base64')
+}
 
+async function sendEmail(getFieldName: Function, file: any) {
   sendgrid.setApiKey(apiKey)
 
-  const msg = {
+  const message = {
     to,
     from,
     templateId,
     dynamic_template_data: {
-      name: 'John',
-      age: '35',
-      linkedin: 'https://linkedin.com/in/guilhermepontes',
-      country: 'Brazil',
-      email: 'behehe@g.com',
+      name: getFieldName('name'),
+      email: getFieldName('email'),
+      country: getFieldName('country'),
+      linkedin: getFieldName('linkedin'),
+      github: getFieldName('github'),
     },
+    attachments: [
+      {
+        content: encode(file.path),
+        filename: file.originalFilename,
+        type: 'application/pdf',
+        disposition: 'attachment',
+      }
+    ]
   }
 
-  const data = await sendgrid.send(msg)
+  void(message);
 
-  res.json({
-    data,
+  // sendgrid.send(message)
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    return res.status(404).json({
+      response: 'API call not found'
+    })
+  }
+
+  const form = new multiparty.Form()
+  const getFieldName = (field: any) => (name: string) => field[name][0]
+
+  form.parse(req, function(err: any, fields: any, files: any) {
+    if (err || !files || !files.file) return res.json({ response: 'error' })
+
+    sendEmail(getFieldName(fields), files.file[0])
+
+    res.json({ response: 'success' })
   })
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 }
