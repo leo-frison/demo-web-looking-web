77f06ad5858b33936cbb29568b9a3625169cd36e
Guilherme Pontes <guilhermepontes@msn.com>
2019-11-30 22:57:08 +0100

    feat: improve seo

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -16,6 +16,12 @@ const Layout: React.FunctionComponent<Props> = ({ children }) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" content={SEO.description} />
       <meta name="keywods" content={SEO.keywords.join('')} />
+      <meta property="og:title" content="Techmill" />
+      <meta property="og:site_name" content="Techmill - Next level automation engineering" />
+      <meta property="og:url" content="https://techmill.nl" />
+      <meta property="og:description" content="Best team of Automation Engineers in The Netherlands" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/static/images/techmill-description.jpg" />
       <link href={SEO.images.favicon} rel="shortcut icon" />
     </Head>
 
